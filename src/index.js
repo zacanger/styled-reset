9d65fce363d6240c4b1e17c9b4180312fdf04258
zacanger <zac@zacanger.com>
2018-03-18 09:41:16 -0600

    Build

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { css } from 'styled-components'
 
-const reset = css`
+export const reset = css`
 html, body, div, span, applet, object, iframe,
 h1, h2, h3, h4, h5, h6, p, blockquote, pre,
 a, abbr, acronym, address, big, cite, code,
